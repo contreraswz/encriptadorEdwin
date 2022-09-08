eec14daa3df1d4bbc4e1ad6a66ebdcf473dc10d1
Contreraswz <contreraswz@gmail.com>
2022-09-08 12:40:38 -0600

    juego del ahorcado, falta limiar y ganar/perder

--- script/scriptJuegoAhorcado.js
+++ script/scriptJuegoAhorcado.js
@@ -2,7 +2,11 @@ let letra ="";
 let letrasEncontradas=[];
 let i=0;
 let palabraIncognita="";
+let falla=0;
 const ventanaModal = document.querySelector(".modal");
+var canvas=document.querySelector("canvas");
+var pincel=canvas.getContext("2d");
+
 const txtPalabraEncontrar = document.querySelector(".txtPalabraEncontrar");
 const areaAhorcadoPalabra = document.querySelector(".areaAhorcadoPalabra");
 const areaLetraUsada = document.querySelector(".areaLetraUsada");
@@ -51,10 +55,16 @@ txtLetraLeer.addEventListener("keyup",()=>{
                 break;
             }
         } else{
+        
+                falla++;
                 elementoNuevoP.style.color="#ff6905";
                 elementoNuevoP.textContent=letra+" > !FALLASTE¡";
                 areaLetraUsada.appendChild(elementoNuevoP);
+                pintarAhorcado(falla);
                 txtLetraLeer.value="";
+                       
+            
+            break; 
         } 
     }
 });
@@ -74,4 +84,59 @@ function pintarEspacios(){
         const elementoNuevoP = document.createElement("p");
         areaAhorcadoPalabra.appendChild(elementoNuevoP);
     } 
+}
+
+function limpiar() {
+    
+}
+
+function pintarAhorcado(){
+       
+    switch (falla) {
+        case 1:
+            pincel.fillRect(100,142,100,7);
+            pincel.fillRect(100,20,10,122);
+            break;
+        case 2:
+            pincel.fillRect(100,20,65,7);
+            pincel.fillRect(165,20,10,25);
+            break;
+        case 3:
+            pincel.beginPath();
+            pincel.arc(170, 60, 17, 0, Math.PI * 2, true);
+            pincel.stroke();            
+            break;
+        case 4:
+            pincel.beginPath();
+            pincel.moveTo(170, 77);
+            pincel.lineTo(170, 120);
+            pincel.lineTo(150, 140);
+            pincel.moveTo(170, 120);
+            pincel.lineTo(190, 140);
+            pincel.stroke();            
+            break;
+        case 5:
+            pincel.beginPath();
+            pincel.moveTo(150, 90);
+            pincel.lineTo(190, 90);
+            pincel.stroke();            
+            break;
+        case 6:
+            alert("PERDISTE CARNAL");
+            pincel.beginPath();
+            pincel.moveTo(157, 55);
+            pincel.lineTo(167, 65);
+            
+            pincel.moveTo(167, 55);
+            pincel.lineTo(157, 65);
+            
+            pincel.moveTo(172, 55);
+            pincel.lineTo(182, 65);
+            
+            pincel.moveTo(182, 55);
+            pincel.lineTo(172, 65);
+            pincel.stroke();
+            break;
+
+    }
 }
